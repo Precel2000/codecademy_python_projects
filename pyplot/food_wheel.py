5c477d9f2444386010b9e0a8b6e3dda00907b3c2
Dominika Daraż <42532942+Precel2000@users.noreply.github.com>
2020-05-28 19:04:46 +0200

    added bar chart

--- pyplot/food_wheel.py
+++ pyplot/food_wheel.py
@@ -27,3 +27,23 @@ orders = pd.read_csv('orders.csv')
 orders['month'] = orders.date.apply(lambda x: x.split('-')[0])
 avg_order = orders.groupby('month').price.mean().reset_index()
 std_order = orders.groupby('month').price.std().reset_index()
+
+ax = plt.subplot()
+
+#get the desired data
+bar_heights = avg_order.price
+bar_errors = std_order.price
+#plot the data
+plt.bar(range(len(bar_heights)),
+  			bar_heights,
+        yerr=bar_errors,
+        capsize=5)
+
+#label and customize the graph
+ax.set_xticks(range(len(bar_heights)))
+ax.set_xticklabels(['April', 'May', 'June', 'July', 'August', 'September'])
+plt.ylabel('Average Order Amount')
+plt.title('Order Amount over Time')
+
+#render the visualisation
+plt.show()

58b89b028535ab84c9a839868d2796f6b3535b55
Dominika Daraż <42532942+Precel2000@users.noreply.github.com>
2020-05-28 14:01:03 +0200

    pie chart added

--- pyplot/food_wheel.py
+++ pyplot/food_wheel.py
@@ -4,9 +4,19 @@ from matplotlib import pyplot as plt
 #load and inspect data
 restaurants=pd.read_csv('restaurants.csv')
 print(restaurants.head(10))
+
 #count no of cuisines offered
 cuisine_options_count = restaurants.cuisine.nunique()
 print(cuisine_options_count)
-cuisine_counts = restaurants.groupby('cuisine').name.count().reset_index()
 
-print(cuisine_counts)
+#get number of restaurants associated with each cuisine type
+cuisine_counts = restaurants.groupby('cuisine').name.count().reset_index()
+cuisines = cuisine_counts['cuisine'].values 
+counts = cuisine_counts.name.values
+
+#plot the pie chart
+plt.figure(figsize=(10,8))
+plt.pie(counts, labels=cuisines, autopct='%d%%')
+plt.axis('equal')
+plt.title('Number of restaurants per cuisine type')
+plt.show()
